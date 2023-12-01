a2503a68399a08e516e1283b24cc8a40b35ea80d
narjoDev <63086420+narjoDev@users.noreply.github.com>
2023-11-30 23:11:38 -0800

    Day 1 both parts solved

--- day01/solution.rb
+++ day01/solution.rb
@@ -1,17 +1,39 @@
 require_relative '../lib/file_utilities'
 
 EXAMPLE = lines_of(read_file('input_example.txt'))
+EXAMPLE_TWO = lines_of(read_file('example_two.txt'))
 ACTUAL = lines_of(read_file('input_actual.txt'))
 
-def part_one(_input)
-  'foo'
+def recover_value(line)
+  digits = line.scan(/\d/)
+  (digits.first + digits.last).to_i
 end
 
-def part_two(_input)
-  'bar'
+def part_one(input)
+  calibration_values = input.map { |line| recover_value(line) }
+  calibration_values.sum
+end
+
+def recover_including_words(line)
+  digit_words = %w(zero one two three four five six seven eight nine)
+  digits = Array(0..9).map(&:to_s)
+  including_digits = digit_words + digits
+  pattern = Regexp.new("(?=(#{including_digits.join('|')}))")
+  # pattern = /(?=(zero|one|two|three|four|five|six|seven|eight|nine|0|1|2|3|4|5|6|7|8|9))/
+
+  all_matches = line.scan(pattern).flatten
+  keep = [all_matches.first, all_matches.last]
+  keep.map! { |match| digit_words.index(match) || match }
+
+  keep.join.to_i
+end
+
+def part_two(input)
+  input.map { |line| recover_including_words(line) }
+       .sum
 end
 
 answer_one = part_one(ACTUAL)
+overwrite('output.txt', "#{answer_one}\n")
 answer_two = part_two(ACTUAL)
-overwrite('output.txt', answer_one + "\n")
-append_write('output.txt', answer_two + "\n")
+append_write('output.txt', "#{answer_two}\n")
--- day01/solution.test.rb
+++ day01/solution.test.rb
@@ -1,4 +1,16 @@
 require_relative 'solution'
 
-p EXAMPLE
-p part_one(nil)
+# p(recover_value('1abc2') == 12)
+# p(recover_value('pqr3stu8vwx') == 38)
+# p(recover_value('a1b2c3d4e5f') == 15)
+# p(recover_value('treb7uchet') == 77)
+
+# p(part_one(EXAMPLE) == 142)
+p(part_one(ACTUAL) == 55386)
+
+# p(recover_including_words('two1nine') == 29)
+# p(recover_including_words('xtwone3four') == 24)
+p(recover_including_words('z7onetwonec') == 71)
+
+p(part_two(EXAMPLE_TWO) == 281)
+p(part_two(ACTUAL) == 54824)
