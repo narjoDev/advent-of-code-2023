a640c8628495def4ebfb6e306717358b6152eddb
narjoDev <63086420+narjoDev@users.noreply.github.com>
2023-12-01 22:16:29 -0800

    day02 part one solved

---
+++ day02/solution.rb
@@ -0,0 +1,39 @@
+require_relative '../lib/file_utilities'
+
+EXAMPLE_ONE = lines_of(read_file('example_one.txt'))
+EXAMPLE_TWO = lines_of(read_file('example_two.txt'))
+ACTUAL = lines_of(read_file('input_actual.txt'))
+
+COLORS = %w(red green blue)
+HYPOTHETICAL_BAG = { 'red' => 12, 'green' => 13, 'blue' => 14 }
+
+def parse_game(line)
+  id = line.match(/(\d+):/).captures[0].to_i
+  max_colors = Hash.new(0)
+  handfuls = line.split(':')[1]
+  COLORS.each do |color|
+    pattern = Regexp.new("(\\d+) #{color}")
+    quantities_pulled = handfuls.scan(pattern).flatten.map(&:to_i)
+    max_colors[color] = quantities_pulled.max
+  end
+  [id, max_colors]
+end
+
+def possible_from_bag?(game, bag)
+  bag.each { |color, number| return false if game[1][color] > number }
+  true
+end
+
+def part_one(input)
+  games = input.map { |line| parse_game(line) }
+  possible = games.select { |game| possible_from_bag?(game, HYPOTHETICAL_BAG) }
+  ids = possible.map(&:first)
+  ids.sum
+end
+
+def part_two(input)
+  input
+end
+
+overwrite('output.txt', "#{part_one(ACTUAL)}\n")
+# append_write('output.txt', "#{part_two(ACTUAL)}\n")
