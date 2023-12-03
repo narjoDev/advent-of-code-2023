796b79c604974e115ec23f53c97200a502fae141
narjoDev <63086420+narjoDev@users.noreply.github.com>
2023-12-03 10:24:20 -0800

    day03 part two solved

--- day03/solution.rb
+++ day03/solution.rb
@@ -40,9 +40,61 @@ def part_one(input)
   part_numbers.sum
 end
 
+def register_adjacent_stars!(input, digit_row_index, first_digit_index,
+                             last_digit_index, possible_gears)
+  part_number = input[digit_row_index][first_digit_index..last_digit_index].to_i
+
+  window_begin = [digit_row_index - 1, 0].max
+  window_end = [digit_row_index + 1, input.size - 1].min
+
+  (window_begin..window_end).each do |row_to_search_index|
+    row = input[row_to_search_index]
+    left = [first_digit_index - 1, 0].max
+    right = [last_digit_index + 1, row.size - 1].min
+
+    (left..right).each do |char_to_search_index|
+      char = row[char_to_search_index]
+      next unless char.match?(/\*/)
+
+      star_coordinates = [row_to_search_index, char_to_search_index]
+      if possible_gears.key?(star_coordinates)
+        possible_gears[star_coordinates] << part_number
+      else
+        possible_gears[star_coordinates] = [part_number]
+      end
+    end
+  end
+  nil
+end
+
 def part_two(input)
-  input
+  possible_gears = {}
+
+  (0...input.size).each do |row_index|
+    row = input[row_index]
+    first_digit_index = 0
+    while first_digit_index < row.size
+      if row[first_digit_index].match(/\D/)
+        first_digit_index += 1
+      else
+        last_digit_index = first_digit_index
+        last_digit_index += 1 until (last_digit_index + 1) >= row.size || row[last_digit_index + 1].match(/\D/)
+
+        if adjacent_symbol?(input, row_index, first_digit_index,
+                            last_digit_index)
+          register_adjacent_stars!(input, row_index, first_digit_index,
+                                   last_digit_index, possible_gears)
+        end
+        first_digit_index = last_digit_index + 2
+      end
+    end
+  end
+
+  possible_gears
+    .select { |_coordinates, adjacent_parts| adjacent_parts.size == 2 }
+    .map { |_coordinates, adjacent_parts| adjacent_parts.reduce(:*) }
+    .sum
 end
 
 overwrite('output.txt', "#{part_one(ACTUAL)}\n")
-# append_write('output.txt', "#{part_two(ACTUAL)}\n")
+append_write('output.txt', "#{part_two(ACTUAL)}\n")
--- day03/solution.test.rb
+++ day03/solution.test.rb
@@ -2,3 +2,11 @@ require_relative 'solution'
 
 p(part_one(EXAMPLE_ONE) == 4361)
 p(part_one(ACTUAL))
+
+dummy_possible_gears = {}
+register_adjacent_stars!(EXAMPLE_ONE, 0, 0, 2, dummy_possible_gears)
+p(dummy_possible_gears == { [1, 3] => [467] })
+register_adjacent_stars!(EXAMPLE_ONE, 2, 2, 3, dummy_possible_gears)
+p(dummy_possible_gears == { [1, 3] => [467, 35] })
+
+p(part_two(EXAMPLE_ONE) == 467835)
