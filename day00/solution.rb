02f86a6c79942c1ba9719a3cf2ccb3bf63a91d88
narjoDev <63086420+narjoDev@users.noreply.github.com>
2023-12-18 12:49:19 -0800

    Tweak template

--- day00/solution.rb
+++ day00/solution.rb
@@ -1,16 +1,22 @@
 require_relative '../lib/file_utilities'
 
-EXAMPLE_ONE = (File.read('example_one.txt')).split(/\n/)
-EXAMPLE_TWO = (File.read('example_two.txt')).split(/\n/)
 ACTUAL = (File.read('input_actual.txt')).split(/\n/)
+
+# Part One
 
 def part_one(input)
   input
 end
 
+# Part Two
+
 def part_two(input)
   input
 end
 
-# overwrite('output.txt', "#{part_one(ACTUAL)}\n")
-# append_write('output.txt', "#{part_two(ACTUAL)}\n")
+# Output
+
+write_parts = 0
+
+overwrite('output.txt', "#{part_one(ACTUAL)}\n") if write_parts.nonzero?
+append_write('output.txt', "#{part_two(ACTUAL)}\n") if write_parts == 2
--- day00/solution.test.rb
+++ day00/solution.test.rb
@@ -1,3 +1,8 @@
 require_relative 'solution'
+# require 'benchmark'
+
+EXAMPLE_ONE = (File.read('example_one.txt')).split(/\n/)
+EXAMPLE_TWO = (File.read('example_two.txt')).split(/\n/)
 
 p part_one(EXAMPLE_ONE)
+# p(part_one(ACTUAL).to_s == File.read('answer.txt').split(/\n/)[0])
